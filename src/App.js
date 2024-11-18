e8455f037c2fee4aca6ecd47ca7c30266e4a2938
Mitya <dim.maluckow2017@yandex.ru>
2024-11-18 17:14:27 +0300

    created Login and Registration pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import s from "./App.module.css"
 import Content from "./components/Content/Content";
 import Login from "./components/Login/Login";
-
+import Registration from "./components/Registration/Registration";
 function App() {
   return (
     <div className = {s.wrapper}>
       <Content/>
       <Login/>
-      
+      <Registration/>
     </div>
   );
 }
---
+++ src/components/Registration/Registration.jsx
@@ -0,0 +1,44 @@
+
+import s from "./Registration.module.css"
+
+const Registration = () => {
+
+  return (
+    <div className={s.content}>
+      <div className = {s.content_flex}></div>
+        <h2>Регистрация</h2>
+        <div className={s.name}>
+          <div className={s.name_item}>
+            <p className = {s.name_description}>Имя</p>
+            <input placeholder="Введите имя" />
+          </div>
+          <div className={s.name_itemsecond}>
+            <p className={s.name_description}>Фамилия</p>
+            <input placeholder = "Введите фамилию"/>
+          </div>
+      </div>
+      <div className={s.content_wrapper}>
+        <div className={s.email}>
+          <p className={s.name_description}>Ваша эл. почта</p>
+          <input placeholder="Введите  адрес электронной почты" />
+        </div>
+        <div className={s.sex}>
+          <p className={s.sex_description}>Пароль</p>
+          <input placeholder="Введите  пароль" />
+        </div>
+        <div className={s.validation}>
+          <p className={s.validation_description}>Подтвердите пароль</p>
+          <input placeholder="Подтвердите  пароль" />
+        </div>
+      </div>
+      <button className={s.registration_click}>ЗАРЕГИСТРИРОВАТЬСЯ</button>
+      <div className={s.questions}>
+        <p>Уже есть аккаунт? <a href="#">Войти</a></p>
+      </div>
+      
+      
+    </div>
+  )
+}
+
+export default Registration
