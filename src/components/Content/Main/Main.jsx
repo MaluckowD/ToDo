bf732b572487e969d2c40a09da28631fe92c014c
Mitya <dim.maluckow2017@yandex.ru>
2024-11-20 12:20:24 +0300

    inserted a ready made calendar component from tailwind

--- src/components/Content/Main/Main.jsx
+++ src/components/Content/Main/Main.jsx
@@ -1,9 +1,11 @@
 import s from "./Main.module.css"
 import Settings from "./Settings/Settings"
+import Calendar from "./Calendar/Calendar"
 const Main = () => {
 
   return (
     <main className = {s.main}>
+      <Calendar/>
       <Settings/>
     </main>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/components/Login/Login.{js,jsx,ts,tsx}"],
+  content: ["./src/components/Login/Login.{js,jsx,ts,tsx}",
+    "./src/components/Content/Main/Calendar/Calendar.{js,jsx,ts,tsx}" ],
   theme: {
     extend: {},
   },
