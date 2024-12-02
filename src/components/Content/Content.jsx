bd2e135316291020d8774b38722505b35bad11a2
Mitya <dim.maluckow2017@yandex.ru>
2024-12-02 23:37:17 +0300

    started connect backend with frontend

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,11 +3,29 @@ import Main from "./Main/Main"
 import Footer from "./Footer/Footer"
 import s from "./Content.module.css"
 import React, { useState } from 'react';
+import axios from "axios";
+import { Ping, RegistrateUser } from "../../api/api"
 import Kirillloh from "../../images/KirillLoh.jpg"
 const Content = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
+  let User = {
+    "name": "Дима",
+    "surname": "илоиоло",
+    "short_name": "fun",
+    "email": "cerber.fishi",
+    "gender": "male",
+    "password": "12ц345"
+  }
+  axios.post("http://energy-cerber.ru:8000/user/register", User)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  
   return(
     <div className = {s.root}>
       {isModalOpen && (
