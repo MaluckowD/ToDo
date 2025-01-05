89bb7f739908ad2b5fab787dfd74eef38a1858cd
Mitya <dim.maluckow2017@yandex.ru>
2025-01-05 23:38:38 +0300

    started refactor the code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./globals.css"
-import store from "./store/redux-store"
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  <Provider store = {store}>
-    <App />
-  </Provider>
+  <App />
 );
 
 
