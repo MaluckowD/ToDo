29b44ed3508eb6104441d4f07aa2df7971bd524e
Igor Chesnykh (notebook) <igor.chesnyx@mail.ru>
2025-02-12 11:11:01 +0300

    Add errors callback

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -41,15 +41,33 @@ const Registration = (props) => {
       return <></>
     }
     setError(null);
-    localStorage.setItem('userData', JSON.stringify(userData));
 
-    console.log('Data saved to localStorage:', userData);
-    setIsConfirmation(true)
+    try {
+      const response = await axios.get(
+        `https://api.energy-cerber.ru/user/register/verify_code?email=${userData.email}`,
+      )
+      console.log("Response Object", response)
+      console.log("response Code", response.status)
 
-    const response = await axios.get(
-      `https://api.energy-cerber.ru/user/register/verify_code?email=${userData.email}`,
-    )
-    console.log(response)
+      if (response.status === 200 || response.status === 201) {
+        localStorage.setItem('userData', JSON.stringify(userData));
+        console.log('Data saved to localStorage:', userData);
+
+        setIsConfirmation(true)
+      }
+      else {
+        setError("Данный email или короткое имя пользователя уже существует")
+      }
+    }
+    catch (error) {
+      if (error.status === 400 || error.status === 401 || error.status === 403) {
+        setError("Данный email или короткое имя пользователя уже существует")
+      }
+      else {
+        setError("Ошибка отправки кода подтверждения! Попробуйте зарегистрироваться позже!")
+      }
+    }
+
   }
 
   const closeConfirmation = () => {
@@ -115,10 +133,10 @@ const Registration = (props) => {
         }
       }
     }
-    catch(error) {
+    catch (error) {
       setErrorCode(`Неверный код подтверждения`);
     }
-      
+
   };
 
   return (
