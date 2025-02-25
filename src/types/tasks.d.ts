48f5207da1272f65ae036ddc4f2adc90d399d571
Mitya <dim.maluckow2017@yandex.ru>
2025-02-25 23:33:59 +0300

    typing Api

--- src/types/tasks.d.ts
+++ src/types/tasks.d.ts
@@ -1,4 +1,4 @@
-interface ITask {
+export interface ITask {
     id: number;
     name: string;
     description: string;
@@ -8,14 +8,19 @@ interface ITask {
     date: string;
   }
   
-interface ICategory {
+export interface ICategory {
     id: number;
     name: string;
     color: string;
     tasks: Task[];
 }
 
-interface IUser {
+export interface ICategoryAdd {
+    name: string;
+    color: string;
+}
+
+export interface IUser {
     id: number;
     name: string;
     surname: string;
@@ -24,6 +29,17 @@ interface IUser {
     gender: string;
     tasks: Task[];
     categories: Category[];
+}
+
+export interface IUserLogin {
+    email: string;
+    password: string;
+}
+
+export interface IUserData {
+    name: string;
+    surname: string;
+    gender: string;
 }
 
 export interface ITaskData {
@@ -38,3 +54,12 @@ export interface ITaskStatus {
     completed: boolean; 
     statusId: number
 }
+
+export interface IUserDataRegistration {
+    name: string,
+    surname: string,
+    short_name: string,
+    email: string,
+    gender: string,
+    password: string,
+  };
